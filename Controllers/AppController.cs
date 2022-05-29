a82712413c2ce69715668d7e705104c65737d23f
Wandir Pereira Filho <wandir@gmail.com>
2022-05-29 01:54:32 -0300

    Inicializando o repositório

---
+++ Controllers/AppController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index(){
+            //throw new InvalidOperationException();
+            return View();
+        }
+    }
+}
