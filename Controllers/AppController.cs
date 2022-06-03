211220a2d3246a4179001639222784073704a5cc
Wandir Pereira Filho <wandir@gmail.com>
2022-06-03 18:58:14 -0300

    Criando CRUD

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using MovieApp.ViewModel;
 
 namespace MovieApp.Controllers
 {
     public class AppController : Controller
     {
+        public static List<MovieViewModel> _movies = new List<MovieViewModel>();
         public IActionResult Index(){
             //throw new InvalidOperationException();
-            return View();
+            return View(_movies);
         }
 
         //Welcome
@@ -14,5 +16,34 @@ namespace MovieApp.Controllers
             //throw new InvalidOperationException();
             return View();
         }
+        public IActionResult AddOrEdit(Guid id){
+            //throw new InvalidOperationException();
+            var movie =_movies.FirstOrDefault(x => x.Id == id);
+            return View(movie);
+        }
+        
+        [HttpPost]
+        public IActionResult AddOrEdit(MovieViewModel model){
+            //throw new InvalidOperationException();
+            var movie =_movies.FirstOrDefault(x => x.Id == model.Id);
+            if(ModelState.IsValid){
+                if (movie == null){
+                    model.Id = Guid.NewGuid();
+                    _movies.Add(model);  
+                }else{
+                    movie.Genre = model.Genre;
+                    movie.Name = model.Name;
+                }
+                return RedirectToAction(nameof(Index));   
+            }
+            return View();
+        }
+
+       [HttpGet("about")]
+         public IActionResult About(){
+            //throw new InvalidOperationException();
+            return View();
+        }
+
     }
 }
---
+++ ViewModel/MovieViewModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.ViewModel
+{
+    public class MovieViewModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Nombre es requerido!")]
+        [MaxLength(30, ErrorMessage = "Texto demasiado largo!")]
+        public string? Name { get; set;}
+
+        [Required(ErrorMessage = "Genro es requerido!")]
+        [MaxLength(30, ErrorMessage = "Texto demasiado largo!")]
+        public string? Genre { get; set; }
+    }
+}
